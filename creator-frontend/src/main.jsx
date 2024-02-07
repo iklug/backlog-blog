ce720e174ad80c42541521bf09fc606ae0cde785
Ian Klug <ianklug123@gmail.com>
2024-02-07 13:11:04 -0700

    something

--- creator-frontend/src/main.jsx
+++ creator-frontend/src/main.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+// import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import store from './app/store.js'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>,
 );
